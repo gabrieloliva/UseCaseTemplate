52873dc791e2c6397b5ac98bc38f102ba48464f2
Gabriel Oliva <gfpoliva@gmail.com>
2019-03-08 12:58:49 -0300

    Use case template

---
+++ UseCase Template.xctemplate/___FILEBASENAME___UseCase.swift
@@ -0,0 +1,24 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import Foundation
+
+public class ___FILEBASENAMEASIDENTIFIER___ {
+
+    // MARK: INITIALIZERS
+
+    public init() {
+
+    }
+    
+}
+
+extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_useCaseName___UseCaseProtocol {
+
+}
